13deff518c1dde27ada423514a5cf17947ea70a4
mrinal013 <mrinalhaque99@gmail.com>
2023-10-07 00:41:59 +0600

    html init

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+
+
+jQuery(document).ready(function($){
+
+    const swiper = new Swiper('.swiper', {
+        observer: true,
+        observeParents: true,
+        slidesPerView: 2.4,
+        speed: 400,
+        spaceBetween: 40,
+
+    });
+
+
+});
